7d10670a472dbce73b74256d88db9ccd0dd75da4
shlang0301 <sasha_grinchenko@outlook.com>
2019-09-09 18:28:14 +0300

    made edits

--- 21Struct/Program.cs
+++ 21Struct/Program.cs
@@ -6,68 +6,56 @@ using System.Threading.Tasks;
 
 namespace _21Struct
 {
-
-    public struct cd
+    public struct Deck
     {
-        public enum CardSuit : int
+        public enum CardSuit
         {
             Clubs, Diamond, Hearts, Spades
         }
-
-        public enum CardNumber : int
+        public enum CardNumber
         {
             Six = 6, Seven, Eight, Nine, Ten, Jack = 2, Lady, King, Ace = 11
         }
-
-        public static int MakeIdCard(CardSuit suit, CardNumber number)
-        {
-            int cardId = ((int)number) * 4 + ((int)suit);
-            return cardId;
-        }
-
-        public static CardNumber makeCardNumber_fromCardId(int cardId)
+        public static CardNumber makeCardNumber(int cardId)
         {
             return (CardNumber)(cardId / 4);
         }
-
-
-        public static CardSuit MakeCardSuit_fromCardId(int cardNo)
+        public static CardSuit MakeCardSuit(int cardNo)
         {
             return (CardSuit)(cardNo % 4);
         }
-
-        public static int[] CreateAndShuffleDeck()
+        public static int[] CreateDeck()
         {
-            int[] massivId = new int[36];
-
+            int[] arrayId = new int[36];
             for (int i = 0; i < 36; i++)
             {
                 if (i <= 11)
                 {
-                    massivId[i] = i + 8;
+                    arrayId[i] = i + 8;
                 }
                 else
                 {
-                    massivId[i] = i + 12;
+                    arrayId[i] = i + 12;
                 }
-
             }
+            return arrayId;
+        }
+        public static int[] ShuffleDeck()
+        {
+            int[] arrayId = CreateDeck();
 
             Random random = new Random();
 
-            for (int i = massivId.Length - 1; i >= 0; i--)
+            for (int i = arrayId.Length - 1; i >= 0; i--)
             {
                 int j = random.Next(i + 1);
-                int temp = massivId[j];
-                massivId[j] = massivId[i];
-                massivId[i] = temp;
-
+                int temp = arrayId[j];
+                arrayId[j] = arrayId[i];
+                arrayId[i] = temp;
             }
 
-            return massivId;
-
+            return arrayId;
         }
-
     }
 
     public struct Player
@@ -79,16 +67,17 @@ namespace _21Struct
 
         public static int GameMove(int[] array, ref int decrimValue)
         {
+            decrimValue--;
 
-                decrimValue--;
-                Console.Write("{0} {1} - {2} score", cd.makeCardNumber_fromCardId(array[decrimValue]), cd.MakeCardSuit_fromCardId(array[decrimValue]), Convert.ToInt32(cd.makeCardNumber_fromCardId(array[decrimValue])));
-                Console.WriteLine();
-                return array[decrimValue];
+            Console.Write("{0} {1} - {2} score", Deck.makeCardNumber(array[decrimValue]),
+                                                 Deck.MakeCardSuit(array[decrimValue]),
+                                                 Convert.ToInt32(Deck.makeCardNumber(array[decrimValue])));
+
+            Console.WriteLine();
+            return array[decrimValue];
         }
-
         public static int GameMoveWithoutCW(int[] array, ref int decrimValue)
         {
-
             decrimValue--;
             return array[decrimValue];
         }
@@ -96,23 +85,19 @@ namespace _21Struct
 
     public struct Play
     {
-
         public Player player1;
         public Player player2;
 
         public static int DetermineWhoGoeFirst()
-            {
+        {
             Random rand = new Random();
             int whoseFirstMove;
             whoseFirstMove = rand.Next(0, 2);
             Console.WriteLine("Player{0} is doing first move!", whoseFirstMove + 1);
             Console.WriteLine();
-            return  whoseFirstMove;
+            return whoseFirstMove;
+        }
     }
-       
-    }
-
-   
 
     class Program
     {
@@ -121,27 +106,21 @@ namespace _21Struct
             Play p;
 
             p.player1 = new Player() { playerName = "Player1" };
-            p.player2 = new Player() { playerName = "Player2" };            
+            p.player2 = new Player() { playerName = "Player2" };
 
-            
-
-            string flagContinueGame = "yes";
-
-            int tempValueForLatestCW=0;
+            int flagContinueGame = 1;
+            int tempValueForLatestCW = 0;
 
             p.player1.scoreWins = 0;
             p.player2.scoreWins = 0;
 
-            while (flagContinueGame == "yes")
+            while (flagContinueGame != 0)
             {
                 Console.Clear();
 
                 int whoseFirstMove = Play.DetermineWhoGoeFirst();
-
-                int[] array = cd.CreateAndShuffleDeck();
-
+                int[] array = Deck.ShuffleDeck();
                 int issuedCardPosition = 36;
-
                 int issuedCardValue = 0;
 
                 p.player1.scoreInGame = 0;
@@ -154,21 +133,21 @@ namespace _21Struct
                 {
                     Console.WriteLine("First player took cards:");
 
-                    issuedCardValue = Convert.ToInt32(cd.makeCardNumber_fromCardId(Player.GameMove(array, ref issuedCardPosition)));
+                    issuedCardValue = Convert.ToInt32(Deck.makeCardNumber(Player.GameMove(array, ref issuedCardPosition)));
                     p.player1.scoreInGame += issuedCardValue;
                     if (issuedCardValue == 11) { p.player1.scoreAcesInGame++; }
 
-                    issuedCardValue = Convert.ToInt32(cd.makeCardNumber_fromCardId(Player.GameMove(array, ref issuedCardPosition)));
+                    issuedCardValue = Convert.ToInt32(Deck.makeCardNumber(Player.GameMove(array, ref issuedCardPosition)));
                     p.player1.scoreInGame += issuedCardValue;
                     if (issuedCardValue == 11) { p.player1.scoreAcesInGame++; }
 
                     Console.WriteLine("Second player took cards:");
 
-                    issuedCardValue = Convert.ToInt32(cd.makeCardNumber_fromCardId(Player.GameMove(array, ref issuedCardPosition)));
+                    issuedCardValue = Convert.ToInt32(Deck.makeCardNumber(Player.GameMove(array, ref issuedCardPosition)));
                     p.player2.scoreInGame += issuedCardValue;
                     if (issuedCardValue == 11) { p.player2.scoreAcesInGame++; }
 
-                    issuedCardValue = Convert.ToInt32(cd.makeCardNumber_fromCardId(Player.GameMove(array, ref issuedCardPosition)));
+                    issuedCardValue = Convert.ToInt32(Deck.makeCardNumber(Player.GameMove(array, ref issuedCardPosition)));
                     p.player2.scoreInGame += issuedCardValue;
                     if (issuedCardValue == 11) { p.player2.scoreAcesInGame++; }
                 }
@@ -176,19 +155,19 @@ namespace _21Struct
                 {
                     Console.WriteLine("Second player took cards:");
 
-                    issuedCardValue = Convert.ToInt32(cd.makeCardNumber_fromCardId(Player.GameMove(array, ref issuedCardPosition)));
+                    issuedCardValue = Convert.ToInt32(Deck.makeCardNumber(Player.GameMove(array, ref issuedCardPosition)));
                     p.player2.scoreInGame += issuedCardValue;
                     if (issuedCardValue == 11) { p.player2.scoreAcesInGame++; }
 
-                    issuedCardValue = Convert.ToInt32(cd.makeCardNumber_fromCardId(Player.GameMove(array, ref issuedCardPosition)));
+                    issuedCardValue = Convert.ToInt32(Deck.makeCardNumber(Player.GameMove(array, ref issuedCardPosition)));
                     p.player2.scoreInGame += issuedCardValue;
                     if (issuedCardValue == 11) { p.player2.scoreAcesInGame++; }
 
                     Console.WriteLine("First player took cards:");
-                    issuedCardValue = Convert.ToInt32(cd.makeCardNumber_fromCardId(Player.GameMove(array, ref issuedCardPosition)));
+                    issuedCardValue = Convert.ToInt32(Deck.makeCardNumber(Player.GameMove(array, ref issuedCardPosition)));
                     p.player1.scoreInGame += issuedCardValue;
                     if (issuedCardValue == 11) { p.player1.scoreAcesInGame++; }
-                    issuedCardValue = Convert.ToInt32(cd.makeCardNumber_fromCardId(Player.GameMove(array, ref issuedCardPosition)));
+                    issuedCardValue = Convert.ToInt32(Deck.makeCardNumber(Player.GameMove(array, ref issuedCardPosition)));
                     p.player1.scoreInGame += issuedCardValue;
                     if (issuedCardValue == 11) { p.player1.scoreAcesInGame++; }
                 }
@@ -198,18 +177,16 @@ namespace _21Struct
                 Console.WriteLine("Player1 - {0} score/ Player2 - {1} score", p.player1.scoreInGame, p.player2.scoreInGame);
                 Console.WriteLine();
 
-               
-
                 for (; ; )
                 {
-                    
-                    if (p.player1.scoreInGame == 21 || p.player1.scoreAcesInGame == 22 || (p.player2.scoreInGame > 21 && p.player1.scoreInGame < p.player2.scoreInGame))
+
+                    if (p.player1.scoreAcesInGame == 2|| p.player1.scoreInGame == 21  || (p.player2.scoreInGame > 21 && p.player1.scoreInGame < p.player2.scoreInGame && p.player2.scoreAcesInGame != 2) )
                     {
                         Console.WriteLine("Player 1 won!");
                         p.player1.scoreWins++;
                         break;
                     }
-                    else if (p.player2.scoreInGame == 21 || p.player2.scoreAcesInGame == 22 || (p.player1.scoreInGame > 21 && p.player2.scoreInGame < p.player1.scoreInGame))
+                    else if (p.player2.scoreAcesInGame == 2 || p.player2.scoreInGame == 21 ||  (p.player1.scoreInGame > 21 && p.player2.scoreInGame < p.player1.scoreInGame && p.player1.scoreAcesInGame != 2))
                     {
                         Console.WriteLine("Player 2 won!");
                         p.player2.scoreWins++;
@@ -229,7 +206,7 @@ namespace _21Struct
                             else
                             {
                                 Console.WriteLine("Player1 took card:");
-                                issuedCardValue = Convert.ToInt32(cd.makeCardNumber_fromCardId(Player.GameMove(array, ref issuedCardPosition)));
+                                issuedCardValue = Convert.ToInt32(Deck.makeCardNumber(Player.GameMove(array, ref issuedCardPosition)));
                                 p.player1.scoreInGame += issuedCardValue;
                                 if (issuedCardValue == 11) { p.player1.scoreAcesInGame++; }
                             }
@@ -247,7 +224,7 @@ namespace _21Struct
                             {
                                 Console.WriteLine("Player2 adds one card!");
                                 Console.WriteLine("Player2 took card:");
-                                issuedCardValue = Convert.ToInt32(cd.makeCardNumber_fromCardId(Player.GameMove(array, ref issuedCardPosition)));
+                                issuedCardValue = Convert.ToInt32(Deck.makeCardNumber(Player.GameMove(array, ref issuedCardPosition)));
                                 p.player2.scoreInGame += issuedCardValue;
                                 if (issuedCardValue == 11) { p.player2.scoreAcesInGame++; }
                             }
@@ -266,10 +243,10 @@ namespace _21Struct
                             else
                             {
                                 Console.WriteLine("Player2 adds one card!");
-                               issuedCardValue = Convert.ToInt32(cd.makeCardNumber_fromCardId(Player.GameMoveWithoutCW(array, ref issuedCardPosition)));
-                               tempValueForLatestCW = array[issuedCardPosition];
-                               p.player2.scoreInGame += issuedCardValue;
-                               if (issuedCardValue == 11) { p.player2.scoreAcesInGame++; }
+                                issuedCardValue = Convert.ToInt32(Deck.makeCardNumber(Player.GameMoveWithoutCW(array, ref issuedCardPosition)));
+                                tempValueForLatestCW = array[issuedCardPosition];
+                                p.player2.scoreInGame += issuedCardValue;
+                                if (issuedCardValue == 11) { p.player2.scoreAcesInGame++; }
                             }
 
                             Console.WriteLine("Player1! Make your choice to add one card or not! 0 - now/any other numbers-yes  ");
@@ -283,20 +260,20 @@ namespace _21Struct
                             else
                             {
                                 Console.WriteLine("Player1 took card:");
-                                issuedCardValue = Convert.ToInt32(cd.makeCardNumber_fromCardId(Player.GameMove(array, ref issuedCardPosition)));
+                                issuedCardValue = Convert.ToInt32(Deck.makeCardNumber(Player.GameMove(array, ref issuedCardPosition)));
                                 p.player1.scoreInGame += issuedCardValue;
                                 if (issuedCardValue == 11) { p.player1.scoreAcesInGame++; }
-                            }                            
-                            Console.Write("Player2 took card:{0} {1} - {2} score", cd.makeCardNumber_fromCardId(tempValueForLatestCW), cd.MakeCardSuit_fromCardId(tempValueForLatestCW), Convert.ToInt32(cd.makeCardNumber_fromCardId(tempValueForLatestCW)));
+                            }
+                            Console.Write("Player2 took card:{0} {1} - {2} score", Deck.makeCardNumber(tempValueForLatestCW), Deck.MakeCardSuit(tempValueForLatestCW), Convert.ToInt32(Deck.makeCardNumber(tempValueForLatestCW)));
                             Console.WriteLine();
                             Console.WriteLine("Player1 - {0} score/ Player2 - {1} score", p.player1.scoreInGame, p.player2.scoreInGame);
                         }
                     }
                 }
 
-                Console.WriteLine("Do you want continue game? yes/no");
+                Console.WriteLine("Do you want continue game? 0 - now/any other numbers-yes");
 
-                flagContinueGame = Console.ReadLine();
+                flagContinueGame = Convert.ToInt32(Console.ReadLine());
 
             }
 
@@ -304,13 +281,11 @@ namespace _21Struct
 
             Console.ReadLine();
 
-     }        
+        }
 
         static void Main(string[] args)
         {
-
-            GoPlay();       
-
+            GoPlay();
         }
     }
 }
